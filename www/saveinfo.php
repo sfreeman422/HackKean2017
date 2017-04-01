733aa0e3da74e81a31b212d7738a440cbb61e756
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-01 18:30:25 -0400

    saveinfo test 2

--- www/saveinfo.php
+++ www/saveinfo.php
@@ -13,22 +13,22 @@ $userheight=$_GET['uh'];
 $userheight2=$_GET['uh2'];
 $userweight=$_GET['uw'];
 
-echo $userid;
-echo $username;
-echo $userheight;
-echo $userheight2;
-echo $userweight;
+// echo $userid;
+// echo $username;
+// echo $userheight;
+// echo $userheight2;
+// echo $userweight;
 
-// $qry = "select id from user_measurement where username='$username' limit 1";
-// $check_query = mysqli_query($connect,$qry);
-// if(!mysqli_fetch_array($check_query)){
-// 	$new_entry = "INSERT INTO user_measurement (id, username, userheight, userheigh2, userweight) VALUES ($userid,$username,$userheight,$userheight2,$userweight)";
-//     $new_entry_query = mysqli_query($new_entry);
-//     echo "insert new entity.";
-// }
+$qry = "select id from user_measurement where username='$username' limit 1";
+$check_query = mysqli_query($connect,$qry);
+if(!mysqli_fetch_array($check_query)){
+	$new_entry = "INSERT INTO user_measurement (id, username, userheight, userheigh2, userweight) VALUES ($userid,$username,$userheight,$userheight2,$userweight)";
+    $new_entry_query = mysqli_query($new_entry);
+    echo "insert new entity.";
+}
 
-// $update_entry = "UPDATE user_measurement SET id=$userid, username = $username, userheight=$userheight, userheigh2=$userheight2, userweight=$userweight WHERE username = $username";
-// $update_entry_query = mysqli_query($update_entry);
-// echo $update_entry_query->error;
+$update_entry = "UPDATE user_measurement SET id=$userid, username = $username, userheight=$userheight, userheigh2=$userheight2, userweight=$userweight WHERE username = $username";
+$update_entry_query = mysqli_query($update_entry);
+echo $update_entry_query->error;
 
 ?>
