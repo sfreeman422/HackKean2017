69812a55f6f04b1bbbfdc8f860dfb15a92a62eb6
HAMBO123 <xgamers.us@gmail.com>
2017-04-01 11:08:33 -0700

    Test

---
+++ methods.py
@@ -0,0 +1,8 @@
+"""
+Use this script to define methods, basicly acts like our base code. 
+We need to put the scripts in here so server.py and client.js can pull
+"""
+
+
+def count(number):
+    return int(number) + 1
---
+++ server.py
@@ -0,0 +1,56 @@
+"""
+really im going to leave this be... It works and im not 100% sure on it
+"""
+import argparse
+import json
+import os
+import traceback
+import webbrowser
+import tornado.websocket
+import tornado.web
+import methods
+
+
+class IndexHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.render("index.html", port=args.port)
+
+
+class WebSocket(tornado.websocket.WebSocketHandler):
+
+    def on_message(self, message):
+        """Evaluates the function pointed to by json-rpc."""
+        json_rpc = json.loads(message)
+
+        try:
+            # The only available method is `count`, but I'm generalizing
+            # to allow other methods without too much extra code
+            result = getattr(methods,
+                             json_rpc["method"])(**json_rpc["params"])
+            error = None
+        except:
+            # Errors are handled by enabling the `error` flag and returning a
+            # stack trace. The client can do with it what it will.
+            result = traceback.format_exc()
+            error = 1
+
+        self.write_message(json.dumps({"result": result, "error": error,
+                                       "id": json_rpc["id"]},
+                                      separators=(",", ":")))
+
+
+parser = argparse.ArgumentParser(description="Starts a webserver for stuff.")
+parser.add_argument("--port", type=int, default=8000, help="The port on which "
+                    "to serve the website.")
+args = parser.parse_args()
+
+handlers = [(r"/", IndexHandler), (r"/websocket", WebSocket),
+            (r'/static/(.*)', tornado.web.StaticFileHandler,
+             {'path': os.path.normpath(os.path.dirname(__file__))})]
+application = tornado.web.Application(handlers)
+application.listen(args.port)
+
+webbrowser.open("http://localhost:%d/" % args.port, new=2)
+
+tornado.ioloop.IOLoop.instance().start()
