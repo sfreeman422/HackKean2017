6daa64c929acc7b6fc5e64e9aed4cb2eb61958cf
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-01 18:49:38 -0400

    saveinfo test 10

--- www/saveinfo.php
+++ www/saveinfo.php
@@ -23,14 +23,14 @@ $qry = "select id from user_measurement where username='$username' limit 1";
 $check_query = mysqli_query($connect,$qry);
 if(!mysqli_fetch_array($check_query)){
 	$new_entry = "INSERT INTO user_measurement (id, username, userheight, userheigh2, userweight) VALUES ($userid,'$username',$userheight,$userheight2,$userweight)";
-    $new_entry_query = mysqli_query($new_entry);
+    $new_entry_query = mysqli_query($connect, $new_entry);
     echo "insert new entity. <br>";
     echo "$new_entry <br>";
     var_dump( $new_entry_query);
 }
 
 $update_entry = "UPDATE user_measurement SET id=$userid, username = $username, userheight=$userheight, userheigh2=$userheight2, userweight=$userweight WHERE username = $username";
-$update_entry_query = mysqli_query($update_entry);
+$update_entry_query = mysqli_query($connect, $update_entry);
 echo $update_entry_query->error;
 
 ?>
