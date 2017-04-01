666c949b4b51b4d78c9a692a1968756a9cdcb28c
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-01 18:54:10 -0400

    body measurement rearrange

--- www/body_measurement.php
+++ www/body_measurement.php
@@ -59,9 +59,9 @@ else{
         <div class="col-sm-8 col-md-9 main">
             <p>Let us know you more: </p>
             <form method="post" enctype="multipart/form-data" action="">
-                <div class="col-sm-1 col-md-2"><input type="text" name="userheight" id="userheight" ></div>
-                <div class="col-sm-1 col-md-2"><input type="text" name="userheight2" id="userheight2" ></div>
-                <div class="col-sm-2 col-md-3"><input type="text" name="userweight" id="userweight" ></div>
+                <div class="col-sm-1 col-md-1"><input type="text" name="userheight" id="userheight" >Height: </div>'
+                <div class="col-sm-1 col-md-1"><input type="text" name="userheight2" id="userheight2" ></div>"
+                <p><div class="col-sm-2 col-md-3"><input type="text" name="userweight" id="userweight" ></div></p>
                 <div class="col-sm-2 col-md-3"><input type="button" onclick = "save_measurement()" class="btn btn-info" value="Save" name="submit"></div>
 
         </div>
