a97306edffb9a142689ae23c1d66c0ae69556ddc
debuggedBrain <ashhelal@gmail.com>
2017-04-01 15:16:34 -0400

    base code

--- methods.py
+++ methods.py
@@ -1,8 +1,9 @@
 """
-Use this script to define methods, basicly acts like our base code. 
+Use this script to define methods, basicly acts like our base code.
 We need to put the scripts in here so server.py and client.js can pull
 """
-
-
-def count(number):
-    return int(number) + 1
+import wolframalpha
+client = wolframalpha.Client("YGJTEJ-36E935EG6H")
+item = raw_input("What is the item: ")
+res = client.query('How many calories in ' + item)
+print(next(res.results).text)
