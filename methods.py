447abb8220c745e5cde1be8efa39b90d7b4b0a60
HAMBO123 <xgamers.us@gmail.com>
2017-04-01 16:17:02 -0400

    Start

--- methods.py
+++ methods.py
@@ -5,23 +5,9 @@ We need to put the scripts in here so server.py and client.js can pull
 import wolframalpha
 client = wolframalpha.Client("YGJTEJ-36E935EG6H")
 item = input("What is the item: ")
-
-x = 'sodium calorie'
-item1, item2 = x.split(' ')
-
-def nutrition(x):
-    if x = 'calorie':
-        question = 'How many calories in '
-    elif x = 'carbohydrates':
-        question = 'How many grams of carbohydrates in '
-    elif x = 'sodium':
-        question = 'How many grams of sodium in '
-    elif x = 'calorie sodium':
-        list = x.split()
-        for word in list:
-item1, item2 = x.split(' ')
-
-
-    for i in range(len(list)):
-        res = client.query(question + item)
-        print(next(res.results).text)
+question1 = "How many grams of sugar are in"
+question2 = "How many grams of fat are in"
+res1 = client.query(question1 + item)
+res2 = client.query(question2 + item)
+print(next(res1.results).text)
+print(next(res2.results).text)
