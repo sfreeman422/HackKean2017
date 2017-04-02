21a09a1213aa6c9f0eff0f3569666b58344e7729
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-02 05:37:48 -0400

    result 7

--- www/result.php
+++ www/result.php
@@ -13,8 +13,31 @@ else{
 $qry = "select * from foodtrack where username='$username' ";
 $check_query = mysqli_query($connect,$qry);
 
-while($row = mysql_fetch_array($check_query)){
-    
-}
+$foodname = array();
+$foodcal = array();
+$foodprice = array();
 
+while($row = mysql_fetch_array($check_query)){
+    array_push($foodname, $row['foodname']);
+    array_push($foodcal, $row['foodcal']);
+    array_push($foodprice, $row['price']);
+    echo $foodname; 
+    echo $foodcal;
+    echo $foodprice;
+}
 ?>
+
+<html>
+
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript">
+
+
+    </script>
+
+</head>
+<body>
+
+</body>
+</html>
