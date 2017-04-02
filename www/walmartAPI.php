546975b8fa31f5244251af4cdc9a4923de6f6e0b
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-02 05:26:17 -0400

    bug 4

--- www/walmartAPI.php
+++ www/walmartAPI.php
@@ -44,7 +44,7 @@ for($i = 0; $i < $number; $i++) {
                         console.log(JSON.stringify(data));
                         $("body").append(JSON.stringify(data["items"][0]["name"]));
                         itemname = JSON.stringify(data["items"][0]["name"]);
-                        itemprice = JSON.stringify(data["item"][0]["SalePrice"]);
+                        itemprice = JSON.stringify(data["items"][0]["salePrice"]);
                         getupc(pad(data["items"][0]["upc"],10));
                         // $("body").append(JSON.stringify(data));
                     }, 
