38b51a1c7354ac0b3dce5b508cce0ed234494b38
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-02 04:17:58 -0400

    nap6

--- www/nodephpapi.php
+++ www/nodephpapi.php
@@ -20,5 +20,20 @@ $itemlist = array();
 for($i = 0; $i < $number; $i++) {
     array_push($itemlist, substr($itemid,12 * i,12));
 }
-var_dump($itemlist);
+// var_dump($itemlist);
 ?>
+
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript">
+        var itemlist = <?php echo json_encode($itemlist) ?>;
+        console.log(JSON.stringify(itemlist);
+    </script>
+
+</head>
+
+<body>
+
+</body>
+</html>
