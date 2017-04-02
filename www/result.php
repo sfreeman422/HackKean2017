812d7159cbdef3f6e34b192a36a08739ca159a16
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-02 09:39:22 -0400

    result 17

--- www/result.php
+++ www/result.php
@@ -23,58 +23,58 @@ while($row = mysqli_fetch_array($check_query)){
     array_push($foodname, $row['foodname']);
     array_push($foodcal, $row['foodcal']);
     array_push($foodprice, $row['price']);
-    // echo $foodname; 
-    // echo $foodcal;
-    // echo $foodprice;
+    // echo $row['foodname']; 
+    // echo $row['foodcal'];
+    // echo $row['price'];
 }
-?>
 
+// var_dump($foodname);
+?>
 <html>
 
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script type="text/javascript">
-
     var foodname = <?php echo json_encode($foodname) ?>;
     var foodcal = <?php echo json_encode($foodcal) ?>;
     var foodprice = <?php echo json_encode($foodprice) ?>;
     console.log(JSON.stringify(foodname));
     console.log(JSON.stringify(foodcal));
     console.log(JSON.stringify(foodprice));
-    var calsum = 0
-    for(var i=0, len=foodcal.length; i<len; i++){
-    foodcal[i] = parseInt(foodcal[i], 10);
-}
-	for (var i = 0, calsum = 0; i < foodcal.length; calsum += foodcal[i++])
-    	;    
-	console.log(calsum); // 6
-    var pricesum = 0
-    for(var i=0, len=foodprice.length; i<len; i++){
-    foodprice[i] = parseInt(foodprice[i], 10);
-}
-	for (var i = 0, pricesum = 0; i < foodprice.length; pricesum += foodprice[i++])
-    	;    
-	console.log(pricesum); // 6
-function hideAllResponses() {
-    var divs = document.getElementsByTagName('div');
-    for(var i = divs.length; i-- ;) {
-        var div = divs[i];
-        if(div.className === 'response') {
-            div.style.display = 'none';
-        }
-    }
-}
-if(calsum > 2000){
-document.write("Your calorie intake is over the recomeneded value! <br />")
-}
-if(calsum < 2000){
-document.write("Your calorie intake is under the recomeneded value! <br />")
-}
-if(calsum == 2000){
-document.write("Your calorie intake is the recomeneded value! <br />")
-}
-document.write("You have spent:" + pricesum + "<br />")
-document.write(foodname.join(" <br /> "));
+    // var calsum = 0
+    // for(var i=0, len=foodcal.length; i<len; i++){
+    //     foodcal[i] = parseInt(foodcal[i], 10);
+    // }
+	// for (var i = 0, calsum = 0; i < foodcal.length; calsum += foodcal[i++])
+    // 	;    
+	// console.log(calsum); // 6
+    // var pricesum = 0
+    // for(var i=0, len=foodprice.length; i<len; i++){
+    //     foodprice[i] = parseInt(foodprice[i], 10);
+    // }
+	// for (var i = 0, pricesum = 0; i < foodprice.length; pricesum += foodprice[i++])
+    // 	;    
+	// console.log(pricesum); // 6
+    // function hideAllResponses() {
+    //     var divs = document.getElementsByTagName('div');
+    //     for(var i = divs.length; i-- ;) {
+    //         var div = divs[i];
+    //         if(div.className === 'response') {
+    //             div.style.display = 'none';
+    //         }
+    //     }
+    // }
+    // if(calsum > 2000){
+    // document.write("Your calorie intake is over the recomeneded value! <br />")
+    // }
+    // if(calsum < 2000){
+    // document.write("Your calorie intake is under the recomeneded value! <br />")
+    // }
+    // if(calsum == 2000){
+    // document.write("Your calorie intake is the recomeneded value! <br />")
+    // }
+    // document.write("You have spent:" + pricesum + "<br />")
+    // document.write(foodname.join(" <br /> "));
     </script>
 </head>
 <body>
