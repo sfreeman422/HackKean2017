154aae73bbf0e908719227ca327560385c6cc276
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-01 22:50:06 -0400

    wa17

--- www/walmartAPI.php
+++ www/walmartAPI.php
@@ -15,9 +15,9 @@ $itemid = '007874235205';
                 type:"GET", 
                 url: "http://api.walmartlabs.com/v1/search?query="+itemid+"&format=json&apiKey=bt2hkmve2uxc8tmfzwn42kfy", 
                 success: function(data) {
-                        $("body").append(JSON.stringify(data["items"][0]["upc"]));
+                        // $("body").append(JSON.stringify(data["items"][0]["upc"]));
                         getupc(pad(data["items"][0]["upc"],10));
-                        // $("body").append(JSON.stringify(data));
+                        $("body").append(JSON.stringify(data));
                     }, 
                 error: function(jqXHR, textStatus, errorThrown) {
                         alert(jqXHR.status);
@@ -37,7 +37,7 @@ $itemid = '007874235205';
                 type:"GET", 
                 url: "https://api.nutritionix.com/v1_1/item?upc="+upcnumber+"&appId=27b8a449&appKey=2480417aee6635ea422d5bd2c05376b8", 
                 success: function(data) {
-                        console.log(JSON.stringify(data));
+                        // console.log(JSON.stringify(data));
                         $("body").append(JSON.stringify(data));
                     }, 
                 error: function(jqXHR, textStatus, errorThrown) {
