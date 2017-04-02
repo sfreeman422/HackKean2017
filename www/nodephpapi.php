d82f20e48b1f57bbd18f9e379ff3f94247267ab2
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-02 02:52:25 -0400

    npa 2

--- www/nodephpapi.php
+++ www/nodephpapi.php
@@ -1,17 +1,17 @@
 <?php
 
-$number = $_GET['number'];
-$itemid = $_GET['itemid'];
+$number = $_POST['number'];
+$itemid = $_POST['itemid'];
 
-session_start();
-if(isset($_SESSION['userid'])){
-    require_once('./classes/dbConnector.php');
-    $userid = $_SESSION['userid'];
-    $username = $_SESSION['username'];
-}
-else{
-    header("Location:index.php"); exit();
-}
+// session_start();
+// if(isset($_SESSION['userid'])){
+//     require_once('./classes/dbConnector.php');
+//     $userid = $_SESSION['userid'];
+//     $username = $_SESSION['username'];
+// }
+// else{
+//     header("Location:index.php"); exit();
+// }
 
 echo $number;
 echo $itemid;
