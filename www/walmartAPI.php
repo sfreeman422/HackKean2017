1ada13de3678b953c452328c7e6492135392dde5
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-01 21:47:06 -0400

    wa4

--- www/walmartAPI.php
+++ www/walmartAPI.php
@@ -7,11 +7,12 @@ $itemid = "007874235205";
 
 <html>
 <head>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script type="text/javascript">
-        google.charts.load('current', {packages: ['corechart', 'line']});
-        google.charts.setOnLoadCallback(drawBasic);
-        function drawBasic() {
+        $(document).ready(function () {
+            getjson();
+        });
+        function getjson() {
             var itemid = "<?php echo $itemid ?>";
             var jsonData = $.ajax({
                 url: "http://api.walmartlabs.com/v1/search?query="+itemid+"&format=json&apiKey=bt2hkmve2uxc8tmfzwn42kfy",
