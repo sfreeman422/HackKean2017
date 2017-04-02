1bccecfaf8abbabf009c8ceb7cb229286b6801c3
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-02 02:33:56 -0400

    wa 33

--- www/walmartAPI.php
+++ www/walmartAPI.php
@@ -1,8 +1,10 @@
 <?php
 
 // $itemid = '007874235205';
-$itemid = $_GET['itemid'];
+// $itemid = $_GET['itemid'];
 // echo $itemid;
+$number = $_POST['number'];
+$itemid = $_POST['itemid'];
 ?>
 
 <html>
