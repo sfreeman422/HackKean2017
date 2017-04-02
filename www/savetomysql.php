98f62e92a1d0873c761e8fb6f694bab81b313e55
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-02 03:21:06 -0400

    stm 4

--- www/savetomysql.php
+++ www/savetomysql.php
@@ -16,18 +16,9 @@ $itemcal = $_GET['itemcal'];
 echo $itemname;
 echo $itemcal;
 
-$new_entry = "INSERT INTO foodtrack (username, userheight, userheigh2, userweight, age, gender, cal) VALUES ($username,'$username',$userheight,$userheight2,$userweight, $age, $gender, $cal)";
+$new_entry = "INSERT INTO foodtrack (username, foodname, foodcal) VALUES ('$username','$itemname',$itemcal)";
 $check_query = mysqli_query($connect,$new_entry);
-if(!mysqli_fetch_array($check_query)){
-	$new_entry = "INSERT INTO user_measurement (id, username, userheight, userheigh2, userweight, age, gender, cal) VALUES ($userid,'$username',$userheight,$userheight2,$userweight, $age, $gender, $cal)";
-    $new_entry_query = mysqli_query($connect, $new_entry);
-    // echo "insert new entity. <br>";
-    // echo "$new_entry <br>";
-    // var_dump( $new_entry_query);
-}
 
-$update_entry = "UPDATE user_measurement SET id=$userid, username = '$username', userheight=$userheight, userheigh2=$userheight2, userweight=$userweight, age=$age, gender=$gender, cal=$cal WHERE username = '$username' ";
-$update_entry_query = mysqli_query($connect, $update_entry);
 // echo $update_entry;
 echo $update_entry_query->error;
 
