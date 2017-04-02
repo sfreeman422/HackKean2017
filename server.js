b44d8ef10a621f32324b310b21c5644c0322968a
Steven Freeman <sfreeman422@gmail.com>
2017-04-01 23:10:31 -0400

    we are able to parse the strings and get only indices in the array that contain numbers, however, we still have symbols and we still need to get rid of the special symbols and dollar amounts.

--- server.js
+++ server.js
@@ -46,16 +46,77 @@ app.post('/upload', function(req, res){
 		if(err){
 			return res.end("error uploading file. ");
 		}
+
+		// visionClient.readDocument(__dirname+"/private/images/"+fileName)
+		// 	.then((data)=>{
+		// 		 const results = data[1].responses[0].fullTextAnnotation;
+		// 		 const receiptFull = results.text;
+		// 		 console.log(receiptFull);
+		// 		 var splitArr = receiptFull.split(" ");
+		// 		 console.log(splitArr.length);
+		// 		 var numArr = []; 
+		// 		 var letterArr = []; 
+		// 		 for(var i = 0; i < splitArr.length; i++){
+		// 		 	console.log(splitArr[i].charAt(0));
+		// 		 	if(splitArr[i].charAt(0) == "0" || splitArr[i].charAt(0) == "1" ||splitArr[i].charAt(0) == "2" || splitArr[i].charAt(0) == "3" || splitArr[i].charAt(0) == "4" || splitArr[i].charAt(0) == "5" || splitArr[i].charAt(0) == "6" || splitArr[i].charAt(0) == "7" || splitArr[i].charAt(0) == "8" || splitArr[i].charAt(0) == "9"){
+		// 		 		numArr.push(splitArr[i].parseInt());
+		// 		 	}
+		// 		 	else{
+		// 		 		letterArr.push(splitArr[i]);
+		// 		 	}
+		// 		 	console.log("Number is: "+i);
+		// 		 }
+		// 		 console.log(splitArr);
+		// 		 console.log(numArr);
+		// 	});
+
+
+
 	visionClient.detectText(__dirname+"/private/images/"+fileName, options, function(err,text,apiResponse){
 		if(err)throw err;
 		//Holds the full returned value from Vision API
 		var receiptFull = text[0].desc;
-		var splitArr = [];
+		console.log(typeof receiptFull);
 		var idArr = [];
 		console.log(text[0].desc);
 		//Split the string out by spaces into the splitArr.
-		splitArr.push(receiptFull.split("\\s+"));
-		console.log(splitArr);
+		var splitArr = receiptFull.split(" ");
+		console.log("splitArr length is: "+splitArr.length);
+		console.log("splitArr is: "+splitArr);
+		//Regex to find all upper and lower case numbers. 
+		var alphabetical = new RegExp("^[A-z]+$");
+		//Regex to find all twelve digit numbers. This one relies on the strings being parsed as ints.  
+		var digits = new RegExp("^\d{12}$");
+		//Regex to find any twelve characters in the array.
+		var twelve = new RegExp("^[A-z0-9]{12}$");
+		console.log("array beforehand: "+splitArr);
+		var onlyNumbersArr = [];
+		//Removes the Alphabetical Characters from the array. 
+		for(var i = 0; i < splitArr.length; i++){
+			var newString="";
+			for(var j = 0; j< splitArr[i].length; j++){
+				if(alphabetical.test(splitArr[i].charAt(j))== true){
+					newString += ""; 
+				}
+				else{
+					newString += splitArr[i].charAt(j); 
+				}
+			}
+		console.log("New String is: "+newString);
+		onlyNumbersArr.push(newString);
+		};
+		console.log("array with alpha removed: "+onlyNumbersArr);
+		
+		//Checks for the 12 Digit strings in the array. This doesnt really work because we have strings. If we parseInt, we lose leading zeroes which are required for the query. 
+		for(var i = 0; i < onlyNumbersArr.length; i++){
+			// console.log("checking for 12");
+			// console.log(onlyNumbersArr[i]);
+			// console.log(typeof onlyNumbersArr[i]);
+
+			if(twelve.test(onlyNumbersArr[i]) == true){
+				console.log("12 digit number: "+onlyNumbersArr[i]);
+			}
+		};
 		res.send(text[0].desc);
 	})		
 	});
