10b0e80983af575f91ed924ce61f97969dbc86df
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-02 09:16:08 -0400

    try to retrieve previous

--- www/result.php
+++ www/result.php
@@ -41,15 +41,6 @@ while($row = mysqli_fetch_array($check_query)){
     console.log(JSON.stringify(foodname));
     console.log(JSON.stringify(foodcal));
     console.log(JSON.stringify(foodprice));
-<<<<<<< HEAD
-        var calsum = 0
-    for(var i=0, len=foodcal.length; i<len; i++){
-    foodcal[i] = parseInt(foodcal[i], 10);
-}
-    for (var i = 0, calsum = 0; i < foodcal.length; calsum += foodcal[i++])
-        ;    
-    console.log(calsum); // 6
-=======
     var calsum = 0
     for(var i=0, len=foodcal.length; i<len; i++){
     foodcal[i] = parseInt(foodcal[i], 10);
@@ -57,20 +48,13 @@ while($row = mysqli_fetch_array($check_query)){
 	for (var i = 0, calsum = 0; i < foodcal.length; calsum += foodcal[i++])
     	;    
 	console.log(calsum); // 6
->>>>>>> 2077ceed82c47cfeadd29c0e61840fb051f89641
     var pricesum = 0
     for(var i=0, len=foodprice.length; i<len; i++){
     foodprice[i] = parseInt(foodprice[i], 10);
 }
-<<<<<<< HEAD
-    for (var i = 0, pricesum = 0; i < foodprice.length; pricesum += foodprice[i++])
-        ;    
-    console.log(pricesum); // 6
-=======
 	for (var i = 0, pricesum = 0; i < foodprice.length; pricesum += foodprice[i++])
     	;    
 	console.log(pricesum); // 6
->>>>>>> 2077ceed82c47cfeadd29c0e61840fb051f89641
 function hideAllResponses() {
     var divs = document.getElementsByTagName('div');
     for(var i = divs.length; i-- ;) {
@@ -94,6 +78,6 @@ document.write(foodname.join(" <br /> "));
     </script>
 </head>
 <body>
-<script type="text/javascript" id="WolframAlphaScriptfd71bf9eaeb39fee6a213411d49e412e" src="//www.wolframalpha.com/widget/widget.jsp?id=fd71bf9eaeb39fee6a213411d49e412e"></script>
+
 </body>
 </html>
