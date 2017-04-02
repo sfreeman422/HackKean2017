d0a1e07b540837e1565988c87f73a4c63f36dd08
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-02 04:13:11 -0400

    nap 6

--- www/nodephpapi.php
+++ www/nodephpapi.php
@@ -13,6 +13,12 @@ $itemid = $_GET['itemid'];
 //     header("Location:index.php"); exit();
 // }
 
-echo $number;
-echo $itemid;
+// echo $number;
+// echo $itemid;
+
+$itemlist = array();
+for($i = 0; $i < $number; $i++) {
+    array_push($itemlist, substr($itemid,12 * i,12));
+}
+var_dump($itemlist);
 ?>
