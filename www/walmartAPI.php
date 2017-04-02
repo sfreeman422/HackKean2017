0dc6cd9d06f8b01c252e99241a52f02986bd597e
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-02 00:31:34 -0400

    wa 28

--- www/walmartAPI.php
+++ www/walmartAPI.php
@@ -9,7 +9,7 @@ echo $itemid;
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script type="text/javascript">
-        console.log(<?php echo $itemid ?>);
+        console.log(parseInt(<?php echo $itemid ?>, 10));
         var itemid = pad(<?php echo $itemid ?>, 12);
         console.log(itemid);
         $(document).ready(function () {
