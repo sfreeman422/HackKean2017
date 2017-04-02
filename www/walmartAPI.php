349fb2e3533dfe7a592a7e1aeb0299c5ff5c3ed7
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-01 22:58:42 -0400

    wa18

--- www/walmartAPI.php
+++ www/walmartAPI.php
@@ -11,19 +11,20 @@ $itemid = '007874235205';
     <script type="text/javascript">
         var itemid = pad(<?php echo $itemid ?>, 12);
         $(document).ready(function () {
-            $.ajax({
-                type:"GET", 
-                url: "http://api.walmartlabs.com/v1/search?query="+itemid+"&format=json&apiKey=bt2hkmve2uxc8tmfzwn42kfy", 
-                success: function(data) {
-                        // $("body").append(JSON.stringify(data["items"][0]["upc"]));
-                        getupc(pad(data["items"][0]["upc"],10));
-                        // $("body").append(JSON.stringify(data));
-                    }, 
-                error: function(jqXHR, textStatus, errorThrown) {
-                        alert(jqXHR.status);
-                    },
-            dataType: "jsonp"
-            });
+            getupc(pad(078742352053, 10));
+            // $.ajax({
+            //     type:"GET", 
+            //     url: "http://api.walmartlabs.com/v1/search?query="+itemid+"&format=json&apiKey=bt2hkmve2uxc8tmfzwn42kfy", 
+            //     success: function(data) {
+            //             // $("body").append(JSON.stringify(data["items"][0]["upc"]));
+            //             getupc(pad(data["items"][0]["upc"],10));
+            //             // $("body").append(JSON.stringify(data));
+            //         }, 
+            //     error: function(jqXHR, textStatus, errorThrown) {
+            //             alert(jqXHR.status);
+            //         },
+            // dataType: "jsonp"
+            // });
         });
         function pad(number,n) {
             var str = '' + number;
@@ -33,7 +34,7 @@ $itemid = '007874235205';
             return str;
         }
         function getupc(upcnumber) {
-            console.log(upcnumber);
+            // console.log(upcnumber);
             $.ajax({
                 type:"GET", 
                 url: "https://api.nutritionix.com/v1_1/item?upc="+upcnumber+"&appId=27b8a449&appKey=2480417aee6635ea422d5bd2c05376b8", 
