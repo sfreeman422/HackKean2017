df8f8c761d7bfbec5c529c97b66ce26bc768f9cc
HAMBO123 <xgamers.us@gmail.com>
2017-04-02 01:27:56 -0700

    Button 2

--- www/classes/topbar.php
+++ www/classes/topbar.php
@@ -1,8 +1,8 @@
 <!--container fluid-->
 <nav class="navbar navbar-default navbar-fixed-top"  role="navigation">
-  <div class="container-fluid">
-            <img src="images/sml_title.png" />
+              <img src="images/sml_title.png" />
 
+  <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
