8c69a87129dc325d6e6ff37ff6412ae92b9c1efe
geniuspeterjiang <yuwei.jiang@rutgers.edu>
2017-04-01 22:01:27 -0400

    wa5

--- www/walmartAPI.php
+++ www/walmartAPI.php
@@ -10,17 +10,27 @@ $itemid = "007874235205";
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function () {
-            getjson();
+            $.ajax({
+                type:"GET", 
+                url: "http://api.walmartlabs.com/v1/search?query="+itemid+"&format=json&apiKey=bt2hkmve2uxc8tmfzwn42kfy", 
+                success: function(data) {
+                        $("body").append(JSON.stringify(data));
+                    }, 
+                error: function(jqXHR, textStatus, errorThrown) {
+                        alert(jqXHR.status);
+                    },
+            dataType: "jsonp"
+            });​​​​​​​​​​​​​​​
         });
-        function getjson() {
-            var itemid = "<?php echo $itemid ?>";
-            var jsonData = $.ajax({
-                url: "http://api.walmartlabs.com/v1/search?query="+itemid+"&format=json&apiKey=bt2hkmve2uxc8tmfzwn42kfy",
-                dataType:"json",
-                async: false
-            }).responseText;
+        // function getjson() {
+        //     var itemid = "<?php echo $itemid ?>";
+        //     var jsonData = $.ajax({
+        //         url: "http://api.walmartlabs.com/v1/search?query="+itemid+"&format=json&apiKey=bt2hkmve2uxc8tmfzwn42kfy",
+        //         dataType:"json",
+        //         async: false
+        //     }).responseText;
             
-        }
+        // }
 
     </script>
 
